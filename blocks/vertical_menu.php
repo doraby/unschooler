68be79ddff349e63b377a5096200101dfccde707
Vasil-Ku <vaskuch84@gmail.com>
2020-10-16 17:48:41 +0300

    add files

---
+++ blocks/vertical_menu.php
@@ -0,0 +1,8 @@
+<div class="vertical-menu">
+  <p class="label">unschooler IT</p>
+  <ul class="social">
+    <li><a href=""><img src="../assets/icon/youtube.png" alt=""></a></li>
+    <li><a href=""><img src="../assets/icon/twitter.png" alt=""></a></li>
+    <li><a href=""><img src="../assets/icon/Vector.png" alt=""></a></li>
+  </ul>
+</div>
---
+++ pages/profession.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+  <title>Profession</title>
+</head>
+<body>
+  <div class="wrapper">
+    <?php include ("../blocks/vertical_menu.php");?>
+    <div class="body">
+      <?php include ("../blocks/header.php");?>
+      <main class="profession-main">
+        <div>
+          <div data-spec-display>Выбор</div>
+          <p>Пройди курс профориентации и выбери своё направление</p>
+        </div>
+        <div>
+          <video src=""></video>
+          <ul>
+          </ul>
+          <button><p>arsdgatgsgysdrjd4etseu6</p></button>
+        </div>
+      </main>
+      <?php include ("../blocks/footer.php");?>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
