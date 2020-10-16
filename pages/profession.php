4c89aa20c6ce160a0ccbe9590117e5b3c8abebf7
Vasil-Ku <vaskuch84@gmail.com>
2020-10-16 19:37:22 +0300

    wert235

--- pages/profession.php
+++ pages/profession.php
@@ -2,18 +2,18 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../styles/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
   <title>Profession</title>
 </head>
 <body>
   <div class="wrapper">
-    <?php include ("blocks/vertical_menu.php");?>
+    <?php include ("../blocks/vertical_menu.php");?>
     <div class="body">
-      <?php include ("blocks/header.php");?>
+      <?php include ("../blocks/header.php");?>
       <main class="profession-main">
         <div>
-          <?php include ("blocks/prof-menu.php");?>
+          <?php include ("../blocks/prof-menu.php");?>
           <p>Пройди курс профориентации и выбери своё направление</p>
         </div>
         <div>
@@ -32,8 +32,8 @@
           <button><p>arsdgatgsgysdrjd4etseu6</p></button>
         </div>
       </main>
-      <?php include ("blocks/footer.php");?>
+      <?php include ("../blocks/footer.php");?>
     </div>
   </div>
-  <script src="script.js"></script>
+  <script src="../scripts/script.js"></script>
 </body>
