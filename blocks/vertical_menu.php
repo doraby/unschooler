01546a42139a4d6a877134c848cc689c6bc08f18
SonYk1993 <anti-magedemon@mail.ru>
2020-11-15 20:40:07 +0300

    удалил лишнее

--- blocks/vertical_menu.php
+++
@@ -1,8 +0,0 @@
-<div class="vertical-menu">
-  <p class="label">unschooler IT</p>
-  <ul class="social">
-    <li><a href=""><img src="/assets/icon/youtube.svg" alt=""></a></li>
-    <li><a href=""><img src="/assets/icon/twitter.svg" alt=""></a></li>
-    <li><a href=""><img src="/assets/icon/facebook.svg" alt=""></a></li>
-  </ul>
-</div>
