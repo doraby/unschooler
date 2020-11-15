0473a62ac9a60103441a118d18baed1d47db83c8
SonYk1993 <anti-magedemon@mail.ru>
2020-11-15 19:29:41 +0300

    done all

--- blocks/guidline.php
+++ blocks/guidline.php
@@ -1,11 +1,31 @@
-<div class="btn">
-<button class="button_size_l">
-    qwerty123
-</button>
-<button class="button_size_m">
-    qwerty123
-</button>
-<button class="button_size_s">
-    qwerty123
-</button>
-</div>
+<div>
+        <button class="button_size_l">
+            class="button_size_l"
+        </button>
+     </div>
+     <br><br>
+        <div>
+            <button class="button_size_m">
+                class="button_size_m"
+            </button>
+        </div>
+        <br><br>
+        <div>
+            <button class="button_size_s">
+                class="button_size_s"
+            </button>
+        </div>
+        <br><br>
+        <div class="exapmle">
+            <h1>H1</h1>
+            <h2>H2</h2>
+            <h3>H3</h3>
+            <h4>H4</h4>
+            <p> teg < p >    Text</p>
+            <p class="p_bold">class="p_bold" <br>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, quae.</p>
+            <a href="">teg < a >          Text</a><br><br>
+            <a class="a_large" href="">class="a_large"    Text</a>
+        </div>
+        <br><br>
+        <input type="text" placeholder="input">
+        
