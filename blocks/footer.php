48c2cbe50cb4d41802a67060c39f18e219febfe1
SonYk1993 <anti-magedemon@mail.ru>
2020-11-22 19:47:06 +0300

    add footer

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,2 +1,19 @@
-<footer class="footer">
+<footer>
+  <p>
+    <img src="logo-white.png" alt="">
+  </p>
+  <ul class="footer_first_column">
+    <li><h4>Unschooler</h4></li>
+    <li class="text_size_s"><a href="#">Unschooler97@gmail.com</a></li>
+    <li class="text_size_s"><a href="#">Telegram - @unschooler</a></li>
+  </ul>
+  <ul class="footer_second_column text_size_s">
+    <li><a href="#">Web-design</a></li>
+    <li><a href="#">HTML&CSS</a></li>
+    <li><a href="#">Верстальщик</a></li>
+    <li><a href="#">Frontend Программист</a></li>
+    <li><a href="#">Beckend Программист</a></li>
+    <li><a href="#">Тестировщик</a></li>
+    <li><a href="#">Менеджер проектов</a></li>
+  </ul>
 </footer>
