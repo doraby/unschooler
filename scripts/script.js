1174dd47d4ab01468d779698f505a682cf89124f
Vasil-Ku <vaskuch84@gmail.com>
2020-11-25 15:33:42 +0300

    c

--- scripts/script.js
+++ scripts/script.js
@@ -99,3 +99,16 @@ function setCard() {
     cardList.append(card);
   }
 }*/
+
+var menuIcon = document.getElementsByClassName('menu-icon')[0],
+    menu = document.getElementsByClassName('menu')[0],
+    openedClass = 'menu_opened';
+menuIcon.addEventListener('click', toogleMenu);
+
+function toogleMenu() {
+    if (menu.classList.contains(openedClass)) {
+        menu.classList.remove(openedClass);
+    } else {
+        menu.classList.add(openedClass);
+    }
+}
