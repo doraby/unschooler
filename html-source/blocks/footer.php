9158d77386acdeb4b2055707841bce78c5ae80d5
SonYk1993 <anti-magedemon@mail.ru>
2020-12-07 22:53:16 +0300

    add footer logo.png  edit php

--- html-source/blocks/footer.php
+++ html-source/blocks/footer.php
@@ -1,2 +1,17 @@
 <footer class="footer">
+  <ul class="footer__column-1">
+    <li class="footer__logo"> </li>
+    <li class="footer__text"><a class="footer__link" href="#">Unschooler97@gmail.com</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">Telegram - @unschooler</a></li>
+  </ul>
+  <ul class="footer__column-2">
+    <li class="footer__text"><a class="footer__link" href="#">Web-design</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">HTML&CSS</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">Верстальщик</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">Frontend Программист</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">Backend Программист</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">Тестировщик</a></li>
+    <li class="footer__text"><a class="footer__link" href="#">Менеджер проектов</a></li>
+  </ul>
+
 </footer>
