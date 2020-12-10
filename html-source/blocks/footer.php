a749b245aa9e8bab8352b5cc96729230b91a1cd2
SonYk1993 <anti-magedemon@mail.ru>
2020-12-10 21:43:59 +0300

    rename ul в footer__colomn подправил css

--- html-source/blocks/footer.php
+++ html-source/blocks/footer.php
@@ -1,10 +1,10 @@
 <footer class="footer">
-  <ul class="footer__column-1">
+  <ul class="footer__column">
     <li class="footer__logo"> </li>
     <li class="footer__text"><a class="footer__link" href="#">Unschooler97@gmail.com</a></li>
     <li class="footer__text"><a class="footer__link" href="#">Telegram - @unschooler</a></li>
   </ul>
-  <ul class="footer__column-2">
+  <ul class="footer__column">
     <li class="footer__text"><a class="footer__link" href="#">Web-design</a></li>
     <li class="footer__text"><a class="footer__link" href="#">HTML&CSS</a></li>
     <li class="footer__text"><a class="footer__link" href="#">Верстальщик</a></li>
