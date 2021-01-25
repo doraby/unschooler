a9b09204bfa1061aac164a00098b3b8b4826d342
Vasil-Ku <vaskuch84@gmail.com>
2021-01-25 19:51:11 +0300

    modyfi video select

--- src/components/profession/professionalVideo.js
+++ src/components/profession/professionalVideo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import video1 from './../../video/CuteCat.mp4'
-import video2 from './../../video/Cat2.mp4'
+import video2 from './../../video/Cat1.mp4'
+import video3 from './../../video/Cat2.mp4'
+import video4 from './../../video/Cat3.mp4'
+import video5 from './../../video/Cat4.mp4'
 
 
 
@@ -12,12 +15,12 @@ const ProfessionVideo = (props) => {
     switch (profession) {
       case 'Web-дизайнер': return video1;
       case 'HTML&CSS': return video2;
-      case 'Frontend-Программист': return video1;
-      case 'Backend-Программист': return video2;
-      case 'Тестировщик': return video1;
-      case 'Менеджер проектов': return video2;
-      case 'Менеджер продукта': return video1;
-      case 'Бизнес аналитик': return video2;
+      case 'Frontend-Программист': return video3;
+      case 'Backend-Программист': return video4;
+      case 'Тестировщик': return video5;
+      case 'Менеджер проектов': return video1;
+      case 'Менеджер продукта': return video2;
+      case 'Бизнес аналитик': return video3;
       default: return video1;
     }
   }
