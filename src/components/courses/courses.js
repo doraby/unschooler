d492fa48ad05fb60eafce9b9d6f2b01e8bd39e25
Dovgalenok <drsasha5@gmail.com>
2021-01-30 22:54:45 +0300

    add .eslintcache to gitignore

--- src/components/courses/courses.js
+++ src/components/courses/courses.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import dogImg from './resources/media/dog.jpg'
+
+const dogStyle = {
+    'background': `url(${dogImg}) 50% no-repeat`,
+    width: '900px',
+    height: '506px'
+}
 
 function Courses() {
   return (
     <main>
       <div>
         Courses
+          <div className="dog-box" style={dogStyle}>
+          </div>
       </div>
     </main>
   );
